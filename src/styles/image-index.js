a755601f8d9380954d355367ac05cf3cac37d837
unknown <williamshengchen8@gmail.com>
2023-09-05 19:20:12 -0400

    adjusted profile url

--- src/styles/image-index.js
+++ src/styles/image-index.js
@@ -36,7 +36,7 @@ const kitchenblack2 = "/residential-gallery/kitchen-black/2.jpg";
 const kitchenblack3 = "/residential-gallery/kitchen-black/3.jpg";
 const kitchenblack4 = "/residential-gallery/kitchen-black/4.jpg";
 
-export const christian = '/Profile-tiny.jpg'
+export const profile = '/Profile-tiny.jpg'
 export const lights = "/altered/philosophy-hero.jpg";
 export const icons = {
   phone: phoneCall,
